2cd71581dad7d0f102ca54331fc0f7048f595418
vvillait88 <vvillait88@yahoo.com>
2022-01-19 22:57:13 -0800

    better error catcher

--- src/errors.js
+++ src/errors.js
@@ -21,17 +21,20 @@ const check = (params, basePath, apiKey, type, endpoint) => new Promise((resolve
 });
 
 const errorHandler = (code) => {
-  const errorMessages = {
-    400: 'Request contained either missing or invalid parameters',
-    401: 'Request contained a missing or invalid key',
-    402: 'Payment Required, You have hit your account maximum (all matches used)',
-    404: 'No records were found matching your request',
-    405: 'Request method is not allowed on the requested resource',
-    429: 'An error occurred due to requests hitting the API too quick',
-    500: 'The server encountered an unexpected condition which prevented it from fulfilling the request',
-  };
+  if (code) {
+    const errorMessages = {
+      400: 'Request contained either missing or invalid parameters',
+      401: 'Request contained a missing or invalid key',
+      402: 'Payment Required, You have hit your account maximum (all matches used)',
+      404: 'No records were found matching your request',
+      405: 'Request method is not allowed on the requested resource',
+      429: 'An error occurred due to requests hitting the API too quick',
+      500: 'The server encountered an unexpected condition which prevented it from fulfilling the request',
+    };
 
-  return (`${code} Error: ${errorMessages[code >= 500 && code < 600 ? 500 : code]}`);
+    return (`${code} Error: ${errorMessages[code >= 500 && code < 600 ? 500 : code]}`);
+  }
+  return ('Unknown Error. Please Try Again.');
 };
 
 export { check, errorHandler };
