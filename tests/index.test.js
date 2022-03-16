78fb1206d36c28630294c7787fc485cbb722c912
vvillait88 <vvillait88@yahoo.com>
2022-03-16 09:11:47 -0700

    add to tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -53,7 +53,11 @@ const companyElastic = {
   },
 };
 
-const autocomplete = { field: 'skill', text: 'c++', size: 10 };
+const autocomplete = {
+  field: 'skill',
+  text: 'c++',
+  size: 10,
+};
 
 const company = { name: 'peopledatalabs' };
 
@@ -62,19 +66,24 @@ const location = { location: '455 Market Street, San Francisco, California 94105
 const school = { name: 'university of oregon' };
 
 describe('Person Enrichment', () => {
-  it(`Should Return Person Record for ${phone}`, () => {
+  it(`Should Return Person Record for ${phone}`, (done) => {
     PDLJSClient.person.enrichment({ phone }).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
 
 describe('Person Identify', () => {
-  it(`Should Return Person Record for ${phone}`, () => {
+  it(`Should Return Person Record for ${phone}`, (done) => {
     PDLJSClient.person.identify({ phone: '4155688415' }).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
     });
@@ -82,103 +91,136 @@ describe('Person Identify', () => {
 });
 
 describe('Person Bulk', () => {
-  it(`Should Return Person Records for ${JSON.stringify(records)}`, () => {
+  it(`Should Return Person Records for ${JSON.stringify(records)}`, (done) => {
     PDLJSClient.person.bulk(records).then((data) => {
       expect(data.length).to.equal(2);
+      expect(data).to.be.a('array');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
 
 describe('Person Search', () => {
-  it(`Should Return Person Records for ${personSQL}`, () => {
+  it(`Should Return Person Records for ${personSQL}`, (done) => {
     PDLJSClient.person.search.sql({ searchQuery: personSQL, size: 10 }).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 
-  it(`Should Return Person Records for ${JSON.stringify(personElastic)}`, () => {
+  it(`Should Return Person Records for ${JSON.stringify(personElastic)}`, (done) => {
     PDLJSClient.person.search.elastic({ searchQuery: personElastic, size: 10 }).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
 
 describe('Person Retrieve', () => {
-  it(`Should Return Person Record for ${personID}`, () => {
+  it(`Should Return Person Record for ${personID}`, (done) => {
     PDLJSClient.person.retrieve(personID).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
 
 describe('Company Enrichment', () => {
-  it(`Should Return Company Record for ${website}`, () => {
+  it(`Should Return Company Record for ${website}`, (done) => {
     PDLJSClient.company.enrichment({ website }).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
 
 describe('Company Search', () => {
-  it(`Should Return Company Records for ${companySQL}`, () => {
+  it(`Should Return Company Records for ${companySQL}`, (done) => {
     PDLJSClient.company.search.sql({ searchQuery: companySQL, size: 10 }).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 
-  it(`Should Return Company Records for ${JSON.stringify(companyElastic)}`, () => {
+  it(`Should Return Company Records for ${JSON.stringify(companyElastic)}`, (done) => {
     PDLJSClient.company.search.elastic({ searchQuery: companyElastic, size: 10 }).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
 
 describe('Autocomplete', () => {
-  it(`Should Return Autocomplete Records for ${JSON.stringify(autocomplete)}`, () => {
+  it(`Should Return Autocomplete Records for ${JSON.stringify(autocomplete)}`, (done) => {
     PDLJSClient.autocomplete(autocomplete).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
 
 describe('Cleaner APIs', () => {
-  it(`Should Return Company Cleaner Records for ${JSON.stringify(company)}`, () => {
+  it(`Should Return Company Cleaner Records for ${JSON.stringify(company)}`, (done) => {
     PDLJSClient.company.cleaner(company).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 
-  it(`Should Return Location Cleaner Records for ${JSON.stringify(location)}`, () => {
+  it(`Should Return Location Cleaner Records for ${JSON.stringify(location)}`, (done) => {
     PDLJSClient.location.cleaner(location).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 
-  it(`Should Return School Cleaner Records for ${JSON.stringify(school)}`, () => {
+  it(`Should Return School Cleaner Records for ${JSON.stringify(school)}`, (done) => {
     PDLJSClient.school.cleaner(school).then((data) => {
       expect(data.status).to.equal(200);
+      expect(data).to.be.a('object');
+      done();
     }).catch((error) => {
       expect(error).to.be.a('string');
+      done();
     });
   });
 });
