7c40dacdd0f4cc38cfed0e6a1208763f6d86de13
vvillait88 <vvillait88@yahoo.com>
2023-04-25 20:27:40 -0700

    add support for params in bulk

--- src/endpoints/bulkEnrichment/index.ts
+++ src/endpoints/bulkEnrichment/index.ts
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 import { check, errorHandler } from '../../errors';
 import { BulkPersonEnrichmentParams, BulkPersonEnrichmentResponse } from '../../types/bulk-types';
+import { EnrichmentAdditionalParams } from '../../types/enrichment-types';
 import { parseRateLimitingResponse } from '../../utils/api-utils';
 
-export default (basePath: string, apiKey: string, records: BulkPersonEnrichmentParams) => {
+export default (basePath: string, apiKey: string, records: BulkPersonEnrichmentParams, params?: EnrichmentAdditionalParams) => {
   const headers = {
     'Content-Type': 'application/json',
     'Accept-Encoding': 'gzip',
@@ -14,7 +15,7 @@ export default (basePath: string, apiKey: string, records: BulkPersonEnrichmentP
 
   return new Promise<BulkPersonEnrichmentResponse>((resolve, reject) => {
     check(records, basePath, apiKey, 'Records', 'bulk').then(() => {
-      axios.post<BulkPersonEnrichmentResponse>(`${basePath}/person/bulk`, records, { headers })
+      axios.post<BulkPersonEnrichmentResponse>(`${basePath}/person/bulk`, records, { headers, params })
         .then((response) => {
           resolve(parseRateLimitingResponse(response));
         })
