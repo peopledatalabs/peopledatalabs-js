cd0cb317d88226e69d8a7ca3f2b50e4f041d3b8f
Dion Larson <me@dionlarson.com>
2023-08-01 16:40:32 -0400

    Add strict typechecking with v23 retrieve filters

--- src/types/retrieve-types.ts
+++ src/types/retrieve-types.ts
@@ -1,13 +1,27 @@
 import { BaseResponse } from './api-types';
 import { PersonResponse } from './common-types';
 
-export type RetrieveParams = {
-  id: string;
-} & {
+export type RetrieveFilter = 'job_change' | 'education' | 'location' | 'personal_emails' | 'phone_number' | 'social_profile' | 'work_email';
+
+export type RetrieveMetaParams = {
   pretty?: boolean;
   titlecase?: boolean;
-  filter_updated?: 'job_change' | any;
+  filter_updated?: RetrieveFilter | RetrieveFilter[];
 };
+
+export type ApiRetrieveMetaParams = {
+  pretty?: boolean;
+  titlecase?: boolean;
+  filter_updated?: string;
+};
+
+export type RetrieveParams = {
+  id: string;
+} & RetrieveMetaParams;
+
+export type ApiRetrieveParams = {
+  id: string;
+} & ApiRetrieveMetaParams;
 
 export interface RetrieveResponse extends BaseResponse {
   data: PersonResponse
