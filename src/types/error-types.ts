583d31b9d3c85b2b27e78db4ee09f2f7c61e5884
Dion Larson <me@dionlarson.com>
2023-08-03 22:15:36 -0400

    Include rate limit information on errors (#99)

--- src/types/error-types.ts
+++ src/types/error-types.ts
@@ -1,1 +1,9 @@
+import { RateLimit } from './api-types';
+
 export type ErrorEndpoint = 'enrichment' | 'autocomplete' | 'search' | 'identify' | 'bulk' | 'cleaner' | 'retrieve' | 'jobTitle' | 'skill' | 'ip';
+
+export type PdlError = {
+  message: string;
+  status: number;
+  rateLimit?: RateLimit;
+};
