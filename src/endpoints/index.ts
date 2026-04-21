587f267a6691608d89de6c3c10223ea599b8f9e7
Jbot29 <jerschwartz@gmail.com>
2026-04-21 14:41:28 +0200

    Job Posting: New job posting endpoint (#373)
    * New job posting endpoint
    
    * test fix
    
    * test fix2

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -7,8 +7,9 @@ import cleaner from './cleaner/index.js';
 import enrichment from './enrichment/index.js';
 import enrichmentPreview from './enrichmentPreview/index.js';
 import identify from './identify/index.js';
+import jobPostingSearch from './jobPostingSearch/index.js';
 import jobTitle from './jobTitle/index.js';
 import retrieve from './retrieve/index.js';
 import search from './search/index.js';
 
-export { autocomplete, bulkCompanyEnrichment, bulkEnrichment, bulkRetrieve, changelog, cleaner, enrichment, enrichmentPreview, identify, jobTitle, retrieve, search };
+export { autocomplete, bulkCompanyEnrichment, bulkEnrichment, bulkRetrieve, changelog, cleaner, enrichment, enrichmentPreview, identify, jobPostingSearch, jobTitle, retrieve, search };
